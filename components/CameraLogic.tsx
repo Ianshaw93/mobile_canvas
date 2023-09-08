bf31a85d11ed91979ea9b3ad362335483716ad1e
Ian <ianshaw93@gmail.com>
2023-09-08 14:50:51 +0100

    storing image blob string

--- components/CameraLogic.tsx
+++ components/CameraLogic.tsx
@@ -1,9 +1,14 @@
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera'
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera'
 import React from 'react'
+import { Filesystem, Directory } from '@capacitor/filesystem'
+import { Capacitor } from '@capacitor/core';
 
-const CameraLogic
- = () => {
+const IMAGE_DIR = 'stored-images'; // is this an angular thing?
 
+const CameraLogic= () => {
+  // how to have Platform check?
+  // Capacitor.getPlatform()
+  const isAvailable = Capacitor.isPluginAvailable('Camera');
   const takePicture = async () => {
     const image = await Camera.getPhoto({
       quality: 90,
@@ -13,10 +18,55 @@ const CameraLogic
     });
     console.log("imaage: ", image)
     var imageUrl = image.webPath;
-
+    if (image) {
+      saveImage(image);
+    }
     // Can be set to the src of an image now
     // imageElement.src = imageUrl;
   }
+
+  const saveImage = async (photo: Photo) => {
+      const base64Data = await readAsBase64(photo);
+      console.log("base64Data: ", base64Data)
+
+      const fileName = new Date().getTime() + '.jpeg';
+      const savedFile = await Filesystem.writeFile({
+        directory: Directory.Data,
+        path: `${IMAGE_DIR}/${fileName}`,
+        data: base64Data,
+      })
+      console.log("savedFile: ", savedFile)
+
+      async function readAsBase64(photo: Photo) {
+        if (Capacitor.isNativePlatform()) {
+          // do something
+          const file = await Filesystem.readFile({
+            // @ts-ignore
+            path: photo.path,
+          })
+
+          return file.data
+        } else {
+          // web app
+
+          const response = await fetch(photo.webPath!)
+          const blob = await response.blob()
+
+          return await convertBlobToBase64(blob) as string
+        }
+      }
+      
+    }
+    const convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => { 
+      const reader = new FileReader
+      reader.onerror = reject
+      reader.onload = () => {
+        resolve(reader.result)
+      }
+      reader.readAsDataURL(blob)
+    })
+
+
   return (
     <>
       <div>Camera</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import CameraLogic from '@/components/CameraLogic';
 import PdfPicker from '@/components/PdfPicker';
 import { Inter } from 'next/font/google'
-import { useRef, useState } from 'react';
 // import { defineCustomeElements } from '@ionic/pwa-elements/loader';
 
 const inter = Inter({ subsets: ['latin'] })
